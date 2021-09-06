c07e2fe6105713b91b0c043ffdab55eb56f62395
21r8430 <21r8430@bztf.ch>
2021-09-06 05:42:34 +0000

    verify added

--- demo/src/main/java/com/example/Punkt.java
+++ demo/src/main/java/com/example/Punkt.java
@@ -4,9 +4,16 @@ public class Punkt {
     private double x_koord;
     private double y_koord;
 
+
     public Punkt(double x, double y){
+        if(x<=100 && x >= -100 && y<=100 && y<=-100){
+
         x_koord = x;
         y_koord = y;
+        }
+        else{
+            System.out.println("Ungültige Eingabe, keine Werte unter oder über 100 erlaubt (:");
+        }
     }
 
     public void setX(double x){
